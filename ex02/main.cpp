caf142db8e1696a7a668a04258bcf332e63b87f0
Andrei Kretov <akretov@student.42prague.com>
2025-01-23 17:21:49 +0100

    ex01 seems to be working

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,12 +2,8 @@
 
 int main (void) {
     std::string name = "Trap_1";
-    std::string name2 = "Trap_2";
     ScravTrap Trap_1(name);
-    ScravTrap Trap_2 = Trap_1;
     Trap_1.attack("Johny");
-    Trap_2.takeDamage(50);
-    Trap_2.beRepaired(3);
     Trap_1.takeDamage(8);
     Trap_1.beRepaired(3);
     Trap_1.beRepaired(10000);
